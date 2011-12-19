52cc7c1921c529459ff4367d25bc21141ec93968
Harald Kirschner <hkirschner@mozilla.com>
2011-12-19 13:08:58 -0800

    Fixed a display and interactions bugs. Added Mozilla license.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Labs Tactile PDF.JS Reader.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2___
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
 // 'use strict';
 
 var PR = {};
@@ -91,6 +127,8 @@ PR.Container = new Class({
 		
 		PDFJS.workerSrc = location.pathname + 'js/vendor/pdf-js/src/worker_loader.js';
 
+		PDFJS.disableWorker = true;
+
 		window.addEvent('keydown', this.onKey.bind(this));
 		window.addEvent('resize', this.onResize.bind(this));
 		
@@ -162,6 +200,7 @@ PR.Container = new Class({
 		this.interactive = this.isReady = false;
 		
 		this.pageNum = 1;
+		this.rendering = 0;
 		
 		this.dashboard.setStyle('display', '');
 	},
@@ -212,9 +251,7 @@ PR.Container = new Class({
 			
 			// this.store.set(this.fileURL, buffer.toUnicode());
 			
-			console.time('new PDFJS.PDFDoc');
 			this.doc = new PDFJS.PDFDoc(buffer);
-			console.timeEnd('new PDFJS.PDFDoc');
 			
 			status.setStyle('display', 'none');
 			
@@ -259,10 +296,13 @@ PR.Container = new Class({
 		// TODO Fix for single-page layout
 		this.pageNum = 1 + (this.pageNum - this.pageStart) % this.pageLayout;
 		
-		if (previousLayout != this.pageLayout)
+		if (previousLayout != this.pageLayout) {
+			// console.log('previousLayout !=');
 			this.render();
-		else
+		} else {
+			// console.log('previousLayout ==');
 			this.updateRender();
+		}
 	},
 
 	render: function(direction) {
@@ -275,8 +315,6 @@ PR.Container = new Class({
 				zIndex: this.pageCount - this.pageNum + 100
 			}
 		});
-		
-		console.log(this.currentContainer);
 		
 		for (var i = 0; i < this.pageLayout; i++) {
 			var page = this.pageList[this.pageNum + i];
@@ -733,28 +771,37 @@ PR.Page = new Class({
 		this.container.running++;
 		
 		this.canvasList[this.version] = canvas;
-		
+
+		canvas.setStyle('opacity', 0.4);
+
 		// Render PDF page into canvas context
-		console.time('doc.startRendering ' + this.num + ' / ' + this.version);
+		// console.time('doc.startRendering ' + this.num + ' / ' + this.version);
 		this.doc.startRendering(context, this.onRenderComplete.bind(this));
 		this.doc = null;
-		
-		// if (useWorker) {
-			// this.injectRender();
-		// }
+
+		console.log('onStartRendering', this.num);
+
+		this.injectRender();
 		
 		return this;
 	},
 	
-	onRenderComplete: function() {
-		console.timeEnd('doc.startRendering ' + this.num + ' / ' + this.version);
+	onRenderComplete: function(e) {
+		console.log('onRenderComplete', this.num, e || 'success');
+
+		this.canvasList[this.version].setStyle('opacity', 1);
+
+		// console.timeEnd('doc.startRendering ' + this.num + ' / ' + this.version);
 		
 		this.running.erase(this.version);
 		
-		this.injectRender();
+		// this.injectRender();
 	},
 	
 	position: function() {
+
+		console.log('position ' + this.bounds.positioned || null);
+
 		if (this.bounds.positioned) return;
 		
 		var pageLayout = this.container.pageLayout,
